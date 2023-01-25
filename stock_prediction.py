55baf67c06c78909e50e7c62452e3920cbe31cba
muhsen-d <122864574+muhsen-d@users.noreply.github.com>
2023-01-25 20:40:46 +0330

    Chapter08

--- stock_prediction.py
+++ stock_prediction.py
@@ -1,14 +1,11 @@
 '''
 Source codes for Python Machine Learning By Example 3rd Edition (Packt Publishing)
-Chapter 7 Predicting Stock Price with Regression Algorithms
+Chapter 8  Predicting Stock Price with Artificial Neural Networks
 Author: Yuxi (Hayden) Liu (yuxi.liu.ece@gmail.com)
 '''
 
 
 import pandas as pd
-from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
-from sklearn.preprocessing import StandardScaler
 
 
 def generate_features(df):
@@ -81,108 +78,122 @@ data_train = data.loc[start_train:end_train]
 X_train = data_train.drop('close', axis=1).values
 y_train = data_train['close'].values
 
-print(X_train.shape)
-print(y_train.shape)
 
 data_test = data.loc[start_test:end_test]
 X_test = data_test.drop('close', axis=1).values
 y_test = data_test['close'].values
 
-print(X_test.shape)
-
-
-# First experiment with linear regression
-
+from sklearn.preprocessing import StandardScaler
 scaler = StandardScaler()
-
 X_scaled_train = scaler.fit_transform(X_train)
 X_scaled_test = scaler.transform(X_test)
 
-param_grid = {
-    "alpha": [1e-4, 3e-4, 1e-3],
-    "eta0": [0.01, 0.03, 0.1],
-}
+
+import tensorflow as tf
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense
+
+tf.random.set_seed(42)
 
 
-from sklearn.linear_model import SGDRegressor
-lr = SGDRegressor(penalty='l2', max_iter=1000, random_state=42)
-grid_search = GridSearchCV(lr, param_grid, cv=5, scoring='r2')
-grid_search.fit(X_scaled_train, y_train)
-
-print(grid_search.best_params_)
-
-lr_best = grid_search.best_estimator_
-
-predictions_lr = lr_best.predict(X_scaled_test)
-
-print(f'MSE: {mean_squared_error(y_test, predictions_lr):.3f}')
-print(f'MAE: {mean_absolute_error(y_test, predictions_lr):.3f}')
-print(f'R^2: {r2_score(y_test, predictions_lr):.3f}')
+model = Sequential([
+    Dense(units=32, activation='relu'),
+    Dense(units=1)
+])
 
 
+model.compile(loss='mean_squared_error',
+              optimizer=tf.keras.optimizers.Adam(0.1))
 
-# Experiment with random forest
+model.fit(X_scaled_train, y_train, epochs=100, verbose=True)
 
-param_grid = {
-    'max_depth': [30, 50],
-    'min_samples_split': [2, 5, 10],
-    'min_samples_leaf': [3, 5]
+predictions = model.predict(X_scaled_test)[:, 0]
 
-}
-
-
-from sklearn.ensemble import RandomForestRegressor
-
-rf = RandomForestRegressor(n_estimators=100, n_jobs=-1, max_features='auto', random_state=42)
-grid_search = GridSearchCV(rf, param_grid, cv=5, scoring='r2', n_jobs=-1)
-grid_search.fit(X_train, y_train)
-
-print(grid_search.best_params_)
-rf_best = grid_search.best_estimator_
-
-predictions_rf = rf_best.predict(X_test)
-
-print(f'MSE: {mean_squared_error(y_test, predictions_rf):.3f}')
-print(f'MAE: {mean_absolute_error(y_test, predictions_rf):.3f}')
-print(f'R^2: {r2_score(y_test, predictions_rf):.3f}')
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+print(f'MSE: {mean_squared_error(y_test, predictions):.3f}')
+print(f'MAE: {mean_absolute_error(y_test, predictions):.3f}')
+print(f'R^2: {r2_score(y_test, predictions):.3f}')
 
 
-# Experiment with SVR
 
-param_grid = [
-    {'kernel': ['linear'], 'C': [100, 300, 500],
-     'epsilon': [0.00003, 0.0001]},
-    {'kernel': ['rbf'], 'gamma': [1e-3, 1e-4],
-     'C': [10, 100, 1000], 'epsilon': [0.00003, 0.0001]}
-]
+from tensorboard.plugins.hparams import api as hp
+HP_HIDDEN = hp.HParam('hidden_size', hp.Discrete([64, 32, 16]))
+HP_EPOCHS = hp.HParam('epochs', hp.Discrete([300, 1000]))
+HP_LEARNING_RATE = hp.HParam('learning_rate', hp.RealInterval(0.01, 0.4))
 
 
-from sklearn.svm import SVR
+def train_test_model(hparams, logdir):
+    model = Sequential([
+        Dense(units=hparams[HP_HIDDEN], activation='relu'),
+        Dense(units=1)
+    ])
+    model.compile(loss='mean_squared_error',
+                  optimizer=tf.keras.optimizers.Adam(hparams[HP_LEARNING_RATE]),
+                  metrics=['mean_squared_error'])
+    model.fit(X_scaled_train, y_train, validation_data=(X_scaled_test, y_test), epochs=hparams[HP_EPOCHS], verbose=False,
+              callbacks=[
+                  tf.keras.callbacks.TensorBoard(logdir),  # log metrics
+                  hp.KerasCallback(logdir, hparams),  # log hparams
+                  tf.keras.callbacks.EarlyStopping(
+                      monitor='val_loss', min_delta=0, patience=200, verbose=0, mode='auto',
+                  )
+              ],
+              )
+    _, mse = model.evaluate(X_scaled_test, y_test)
+    pred = model.predict(X_scaled_test)
+    r2 = r2_score(y_test, pred)
+    return mse, r2
 
-svr = SVR()
-grid_search = GridSearchCV(svr, param_grid, cv=2, scoring='r2')
-grid_search.fit(X_scaled_train, y_train)
+def run(hparams, logdir):
+    with tf.summary.create_file_writer(logdir).as_default():
+        hp.hparams_config(
+            hparams=[HP_HIDDEN, HP_EPOCHS, HP_LEARNING_RATE],
+            metrics=[hp.Metric('mean_squared_error', display_name='mse'),
+                     hp.Metric('r2', display_name='r2')],
+        )
+        mse, r2 = train_test_model(hparams, logdir)
+        tf.summary.scalar('mean_squared_error', mse, step=1)
+        tf.summary.scalar('r2', r2, step=1)
 
-print(grid_search.best_params_)
 
-svr_best = grid_search.best_estimator_
+session_num = 0
 
-predictions_svr = svr_best.predict(X_scaled_test)
+for hidden in HP_HIDDEN.domain.values:
+    for epochs in HP_EPOCHS.domain.values:
+        for learning_rate in tf.linspace(HP_LEARNING_RATE.domain.min_value, HP_LEARNING_RATE.domain.max_value, 5):
+            hparams = {
+                HP_HIDDEN: hidden,
+                HP_EPOCHS: epochs,
+                HP_LEARNING_RATE: float("%.2f"%float(learning_rate)),
+            }
+            run_name = "run-%d" % session_num
+            print('--- Starting trial: %s' % run_name)
+            print({h.name: hparams[h] for h in hparams})
+            run(hparams, 'logs/hparam_tuning/' + run_name)
+            session_num += 1
 
 
-print(f'MSE: {mean_squared_error(y_test, predictions_svr):.3f}')
-print(f'MAE: {mean_absolute_error(y_test, predictions_svr):.3f}')
-print(f'R^2: {r2_score(y_test, predictions_svr):.3f}')
 
+model = Sequential([
+    Dense(units=16, activation='relu'),
+    Dense(units=1)
+])
+
+model.compile(loss='mean_squared_error',
+              optimizer=tf.keras.optimizers.Adam(0.21))
+
+model.fit(X_scaled_train, y_train, epochs=1000, verbose=False)
+
+predictions = model.predict(X_scaled_test)[:, 0]
 
 
 import matplotlib.pyplot as plt
 plt.plot(data_test.index, y_test, c='k')
-plt.plot(data_test.index, predictions_lr, c='b')
-plt.plot(data_test.index, predictions_rf, c='r')
-plt.plot(data_test.index, predictions_svr, c='g')
+plt.plot(data_test.index, predictions, c='b')
+plt.plot(data_test.index, predictions, c='r')
+plt.plot(data_test.index, predictions, c='g')
 plt.xticks(range(0, 252, 10), rotation=60)
 plt.xlabel('Date')
 plt.ylabel('Close price')
-plt.legend(['Truth', 'Linear regression', 'Random Forest', 'SVR'])
+plt.legend(['Truth', 'Neural network prediction'])
 plt.show()
